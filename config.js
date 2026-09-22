87ef078bfb83542384b2bb8141cbb565cfc5019a
delsenorinfluencers-creator <delsenorinfluencers@gmail.com>
2026-09-22 09:59:19 -0500

    Add files via upload

--- config.js
+++ config.js
@@ -1,14 +1,10 @@
 window.SUPABASE_URL="https://sqawhmimfcwnvkeviznk.supabase.co";
 window.SUPABASE_PUBLISHABLE_KEY="sb_publishable_IpEAQRQ7To6ONqG6MyWuuA_DM630ufy";
-window.SUPABASE_KEY=window.SUPABASE_PUBLISHABLE_KEY;
 window.JIS_CONFIG={
   logo:"https://i.ibb.co/tw6GTfyV/Dise-o-sin-t-tulo-12.png",
   logo64:"https://i.ibb.co/HfhqmYQs/file-000000006d4c81f5b9452f237ee19824.png",
-  favicon:"https://i.ibb.co/tw6GTfyV/Dise-o-sin-t-tulo-12.png",
   siteName:"Jóvenes Influencers del Señor",
-  siteDescription:"Comunicación al servicio de Dios. Información, formación y evangelización digital desde la Diócesis de Ocaña.",
-  liveUrl:"",
+  siteUrl:"https://influencers-del-senor.vercel.app",
   facebook:"https://www.facebook.com/jovenesinfluencersdelsenor",
-  youtube:"",
-  whatsapp:""
+  liveUrl:""
 };
